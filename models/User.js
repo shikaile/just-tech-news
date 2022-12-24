deb58210bca3026a2929569e57f63ba11c2e2aa5
史开乐 <csurfssecure@gmail.com>
2022-12-23 23:33:52 -0500

    base

---
+++ models/User.js
@@ -0,0 +1,60 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create User model
+class User extends Model {}
+
+User.init(
+    {
+        //table colum definitions go here
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+                  // there cannot be any duplicate email values in this table
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                        // this means the password must be at least four characters long
+                len: [4]
+            }
+        }
+    },
+    {
+    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+
+            // pass in our imported sequelize connection (the direct connection to our database)
+        sequelize,
+
+            // don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+
+            // don't pluralize name of database table
+        freezeTableName: true,
+
+            // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+        underscored: true,
+
+            // make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
