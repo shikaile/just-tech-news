a0e38b7453bd73a79a4d1e1a6c1f2076b0c4884d
史开乐 <csurfssecure@gmail.com>
2022-12-24 19:45:54 -0500

    crypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt')
 
 //create User model
 class User extends Model {}
@@ -21,8 +22,7 @@ User.init(
         email: {
             type: DataTypes.STRING,
             allowNull: false,
-
-                  // there cannot be any duplicate email values in this table
+            // there cannot be any duplicate email values in this table
             unique: true,
             validate: {
                 isEmail: true
@@ -39,7 +39,18 @@ User.init(
     },
     {
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
+        hooks: {
+              // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData){
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                    return newUserData;
+            },
+              // set up beforeCreate lifecycle "hook" functionality
+            async beforeUpdate(updateUserData){
+                updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+                    return updateUserData;
+            }
+        },
             // pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
 
